ef282707120e0634a072f9ec879e712064214d13
Aachman Garg <imaachman@gmail.com>
2025-03-14 13:38:47 +0530

    Fix render overflow in AuthorEditorDialog with on-screen keyboard

--- lib/features/authors/screens/author_editor/author_editor_dialog.dart
+++ lib/features/authors/screens/author_editor/author_editor_dialog.dart
@@ -32,108 +32,110 @@ class _AuthorEditorDialogState extends ConsumerState<AuthorEditorDialog> {
       shape: RoundedRectangleBorder(),
       child: Padding(
         padding: const EdgeInsets.all(32),
-        child: Form(
-          key: formKey,
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text(
-                widget.author == null ? 'Add New Author' : 'Edit Author',
-                style: Theme.of(context)
-                    .textTheme
-                    .headlineMedium
-                    ?.copyWith(fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 16),
-              AspectRatio(
-                aspectRatio: 1,
-                child: model.temporaryProfilePicture.isEmpty
-                    ? Container(
-                        color: Theme.of(context).colorScheme.surface,
-                        child: Center(
-                          child: TextButton.icon(
-                            icon: Icon(Icons.file_upload_outlined),
-                            label: Text('Upload picture'),
-                            onPressed: () => model.selectProfilePicture(),
-                          ),
-                        ),
-                      )
-                    : Stack(
-                        children: [
-                          Positioned.fill(
-                            child: Image.file(
-                              File(model.temporaryProfilePicture),
-                              fit: BoxFit.cover,
+        child: SingleChildScrollView(
+          child: Form(
+            key: formKey,
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Text(
+                  widget.author == null ? 'Add New Author' : 'Edit Author',
+                  style: Theme.of(context)
+                      .textTheme
+                      .headlineMedium
+                      ?.copyWith(fontWeight: FontWeight.bold),
+                ),
+                SizedBox(height: 16),
+                AspectRatio(
+                  aspectRatio: 1,
+                  child: model.temporaryProfilePicture.isEmpty
+                      ? Container(
+                          color: Theme.of(context).colorScheme.surface,
+                          child: Center(
+                            child: TextButton.icon(
+                              icon: Icon(Icons.file_upload_outlined),
+                              label: Text('Upload picture'),
+                              onPressed: () => model.selectProfilePicture(),
                             ),
                           ),
-                          Align(
-                            alignment: Alignment.topRight,
-                            child: Padding(
-                              padding: const EdgeInsets.all(8),
-                              child: SizedBox.square(
-                                dimension: 36,
-                                child: IconButton(
-                                  onPressed: model.clearProfilePicture,
-                                  icon: Icon(Icons.delete_outline, size: 20),
-                                  color:
-                                      Theme.of(context).colorScheme.onSecondary,
-                                  style: ButtonStyle(
-                                    backgroundColor: WidgetStatePropertyAll(
-                                      Theme.of(context)
-                                          .colorScheme
-                                          .secondary
-                                          .withAlpha(120),
+                        )
+                      : Stack(
+                          children: [
+                            Positioned.fill(
+                              child: Image.file(
+                                File(model.temporaryProfilePicture),
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            Align(
+                              alignment: Alignment.topRight,
+                              child: Padding(
+                                padding: const EdgeInsets.all(8),
+                                child: SizedBox.square(
+                                  dimension: 36,
+                                  child: IconButton(
+                                    onPressed: model.clearProfilePicture,
+                                    icon: Icon(Icons.delete_outline, size: 20),
+                                    color:
+                                        Theme.of(context).colorScheme.onSecondary,
+                                    style: ButtonStyle(
+                                      backgroundColor: WidgetStatePropertyAll(
+                                        Theme.of(context)
+                                            .colorScheme
+                                            .secondary
+                                            .withAlpha(120),
+                                      ),
                                     ),
+                                    tooltip: 'Delete cover',
                                   ),
-                                  tooltip: 'Delete cover',
                                 ),
                               ),
                             ),
-                          ),
-                        ],
-                      ),
-              ),
-              NameField(
-                initialValue: model.author?.name ?? widget.query,
-                onChanged: model.setName,
-              ),
-              const SizedBox(height: 16),
-              BioField(
-                initialValue: model.bio,
-                onChanged: model.setBio,
-              ),
-              const SizedBox(height: 16),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  TextButton(
-                    onPressed: () => Navigator.pop(context),
-                    child: Text(
-                      'Cancel',
-                      style: Theme.of(context).textTheme.labelLarge,
-                    ),
-                  ),
-                  SizedBox(width: 16),
-                  TextButton(
-                    style: ButtonStyle(
-                      shape: WidgetStatePropertyAll(RoundedRectangleBorder()),
-                      backgroundColor: WidgetStatePropertyAll(
-                        Theme.of(context).primaryColor,
+                          ],
+                        ),
+                ),
+                NameField(
+                  initialValue: model.author?.name ?? widget.query,
+                  onChanged: model.setName,
+                ),
+                const SizedBox(height: 16),
+                BioField(
+                  initialValue: model.bio,
+                  onChanged: model.setBio,
+                ),
+                const SizedBox(height: 16),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TextButton(
+                      onPressed: () => Navigator.pop(context),
+                      child: Text(
+                        'Cancel',
+                        style: Theme.of(context).textTheme.labelLarge,
                       ),
                     ),
-                    onPressed: () => model.updateAuthor(context, formKey),
-                    child: Text(
-                      widget.author == null ? 'Create Author' : 'Update Author',
-                      style: Theme.of(context).textTheme.labelLarge?.copyWith(
-                            color: Theme.of(context).colorScheme.onPrimary,
-                          ),
+                    SizedBox(width: 16),
+                    TextButton(
+                      style: ButtonStyle(
+                        shape: WidgetStatePropertyAll(RoundedRectangleBorder()),
+                        backgroundColor: WidgetStatePropertyAll(
+                          Theme.of(context).primaryColor,
+                        ),
+                      ),
+                      onPressed: () => model.updateAuthor(context, formKey),
+                      child: Text(
+                        widget.author == null ? 'Create Author' : 'Update Author',
+                        style: Theme.of(context).textTheme.labelLarge?.copyWith(
+                              color: Theme.of(context).colorScheme.onPrimary,
+                            ),
+                      ),
                     ),
-                  ),
-                ],
-              )
-            ],
+                  ],
+                )
+              ],
+            ),
           ),
         ),
       ),
