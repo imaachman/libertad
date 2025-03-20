f4ce5f76c0b3af9bdccc5de69cfc149b7cb8d680
Aachman Garg <imaachman@gmail.com>
2025-03-20 12:33:58 +0530

    Add comments to FilesRepository

--- lib/data/repositories/files_repository.dart
+++ lib/data/repositories/files_repository.dart
@@ -4,17 +4,19 @@ import 'package:file_picker/file_picker.dart';
 import 'package:libertad/data/models/image_folder.dart';
 import 'package:path_provider/path_provider.dart';
 
-/// Repository class to interact with files in the device.
+/// Repository of methods which we can use to interact with device files.
 class FilesRepository {
   /// Private constructor to create a singleton.
   FilesRepository._internal();
 
-  /// We use this instance to utilize files repository.
+  /// We use this instance to provide access to the files repository across the
+  /// app.
   static final FilesRepository instance = FilesRepository._internal();
 
-  /// Select an image file from the device.
+  /// Selects an image file from the device.
   Future<File?> selectImageFile() async {
     // Open file picker to select an image file.
+    // We allow only `jpg`, `jpeg`, and `png` formats.
     final FilePickerResult? result = await FilePicker.platform.pickFiles(
       type: FileType.custom,
       allowedExtensions: ['jpg', 'jpeg', 'png'],
@@ -25,6 +27,7 @@ class FilesRepository {
     return File(result.files.single.path!);
   }
 
+  /// Copies the image file to the app's image directory.
   Future<File> copyImageFile(
       String originalImagePath, ImageFolder folder) async {
     // Get the path to the app's documents directory.
@@ -37,14 +40,15 @@ class FilesRepository {
     // Path to the new image file.
     final String newImagePath =
         '${appImageDirectory.path}/${originalImagePath.split('\\').last}';
-    // Copy the selected image to the app's documents directory.
+    // Copy the selected image to the app's image directory.
     final File copiedFile = await File(originalImagePath).copy(newImagePath);
     return copiedFile;
   }
 
-  /// Delete the file from the app's documents directory.
+  /// Deletes the file from the app directory.
   Future<void> deleteFile(String path) => File(path).delete();
 
+  /// Deletes the image directory provided as [folder].
   Future<void> deleteImageFolder(ImageFolder folder) async {
     // Get the path to the app's documents directory.
     final Directory applicationDocumentsDirectory =
@@ -53,25 +57,33 @@ class FilesRepository {
     final Directory appImageDirectory = await Directory(
             '${applicationDocumentsDirectory.path}/Libertad/${folder.prettify}')
         .create(recursive: true);
+    // Delete the image directory.
     File(appImageDirectory.path).delete(recursive: true);
   }
 
-  /// Replace the old file with the new file.
+  /// Replaces the old file with the new file and returns the new file's path.
   Future<String> replaceFile(
       String oldPath, String newPath, ImageFolder folder) async {
     // Delete the old file.
     await deleteFile(oldPath);
     // Create a copy of the new file.
     final File copiedFile = await copyImageFile(newPath, folder);
+    // Return copied file's path.
     return copiedFile.path;
   }
 
+  /// Exposes app directory by returning the paths to all the files. (for
+  /// development purposes only)
   Future<List<String>> exposeAppDirectoryFiles() async {
+    // Get the path to the app's documents directory.
     final Directory applicationDocumentsDirectory =
         await getApplicationDocumentsDirectory();
+    // Retrieve app directory by appending to the path.
     final Directory appDirectory =
         Directory('${applicationDocumentsDirectory.path}/Libertad');
+    // Retrieve all files in the directory.
     final List<FileSystemEntity> files = appDirectory.listSync(recursive: true);
+    // Return all files' path.
     return files.map((file) => file.path).toList();
   }
 }
