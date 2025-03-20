078817a26d787da032808c2cd638b12878e134e4
Aachman Garg <imaachman@gmail.com>
2025-03-20 14:09:08 +0530

    Update author and borrower search bar theme
    - Make them consistent with database search bar

--- lib/features/book_copies/screens/copy_details_screen/borrowers_search_delegate.dart
+++ lib/features/book_copies/screens/copy_details_screen/borrowers_search_delegate.dart
@@ -3,11 +3,27 @@ import 'package:libertad/data/models/borrower.dart';
 import 'package:libertad/features/book_copies/screens/copy_details_screen/borrowers_search_screen.dart';
 
 class BorrowersSearchDelegate extends SearchDelegate<Borrower?> {
+  BorrowersSearchDelegate() : super(searchFieldLabel: 'Search borrowers');
+
+  @override
+  ThemeData appBarTheme(BuildContext context) {
+    return Theme.of(context).copyWith(
+      textTheme: Theme.of(context)
+          .textTheme
+          .copyWith(titleLarge: Theme.of(context).textTheme.bodyLarge),
+      inputDecorationTheme: InputDecorationTheme(
+        border: InputBorder.none,
+        hintStyle: Theme.of(context).textTheme.bodyLarge,
+        isDense: true,
+      ),
+    );
+  }
+
   @override
   List<Widget>? buildActions(BuildContext context) {
     return [
       IconButton(
-        icon: Icon(Icons.close),
+        icon: Icon(Icons.close, size: 20),
         onPressed: () {
           query = '';
         },
@@ -18,7 +34,7 @@ class BorrowersSearchDelegate extends SearchDelegate<Borrower?> {
   @override
   Widget? buildLeading(BuildContext context) {
     return IconButton(
-      icon: Icon(Icons.arrow_back),
+      icon: Icon(Icons.arrow_back, size: 20),
       onPressed: () => close(context, null),
     );
   }
--- lib/features/books/screens/book_editor/authors_search_delegate.dart
+++ lib/features/books/screens/book_editor/authors_search_delegate.dart
@@ -3,11 +3,27 @@ import 'package:libertad/data/models/author.dart';
 import 'package:libertad/features/books/screens/book_editor/authors_search_screen.dart';
 
 class AuthorsSearchDelegate extends SearchDelegate<Author?> {
+  AuthorsSearchDelegate() : super(searchFieldLabel: 'Search authors');
+
+  @override
+  ThemeData appBarTheme(BuildContext context) {
+    return Theme.of(context).copyWith(
+      textTheme: Theme.of(context)
+          .textTheme
+          .copyWith(titleLarge: Theme.of(context).textTheme.bodyLarge),
+      inputDecorationTheme: InputDecorationTheme(
+        border: InputBorder.none,
+        hintStyle: Theme.of(context).textTheme.bodyLarge,
+        isDense: true,
+      ),
+    );
+  }
+
   @override
   List<Widget>? buildActions(BuildContext context) {
     return [
       IconButton(
-        icon: Icon(Icons.close),
+        icon: Icon(Icons.close, size: 20),
         onPressed: () {
           query = '';
         },
@@ -18,7 +34,7 @@ class AuthorsSearchDelegate extends SearchDelegate<Author?> {
   @override
   Widget? buildLeading(BuildContext context) {
     return IconButton(
-      icon: Icon(Icons.arrow_back),
+      icon: Icon(Icons.arrow_back, size: 20),
       onPressed: () => close(context, null),
     );
   }
